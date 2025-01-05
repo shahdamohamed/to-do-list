16521ea5f1492ba977776f3a7fdd4f7e7ef1f289
Shahda <shahdamohamedabotalb@gmail.com>
2025-01-05 18:15:10 +0200

    feat:add tasks per user

--- main/models.py
+++ main/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from user.models import User
 # Create your models here.
 class Task(models.Model):
     PRIORITY_CHOICES= [
@@ -14,6 +14,7 @@ class Task(models.Model):
     priority=models.CharField(max_length=7, choices=PRIORITY_CHOICES, default='Meduim')
     finished_date=models.DateField(blank=True, null=True)
     created_at=models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.title
